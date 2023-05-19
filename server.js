70d5a3cfb335234b8b760afe8b1f54ea88a51664
nguyenthai98 <nguyenthai492018@gmail.com>
2023-05-19 22:01:41 +0700

    add cors

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express();
 const PORT = 3000;
-const db = require('./config/db.config');
 const bankRoute = require('./routers/bank_route');
 
+
+var cors = require('cors')
+app.use(cors())
  
 // db connect
 //db.connect();
 
 // routers
 app.use(bankRoute); 
+
+
 app.listen(PORT, () => {
     console.log(`start sever at port http://localhost:${PORT}`);
 })
